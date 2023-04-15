6d0475f35f9181de01c3b840ec58c154ddfadc68
Rafaela Rocha Nogueira <rafah_nogueira@hotmail.com>
2023-04-15 15:25:52 -0300

    Criação do projeto inicial

---
+++ src/test/java/com/dev/springbootjsp/SpringbootJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.springbootjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
